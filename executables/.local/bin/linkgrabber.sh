a4173cae21565281456360ed188dc00f41f4e560
Jashaswimalya Acharjee <jashaswimalyaacharjee@gmail.com>
2021-07-24 11:20:16 +0530

    Removed linkgrabber.sh from general executables

--- executables/.local/bin/linkgrabber.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-BROWSER='firefox'
-regex='(((http|https|ftp|gopher)|mailto)[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]'
-url=$(grep -Po "$regex" | dmenu -p "Go:") || exit
-$BROWSER "$url"
